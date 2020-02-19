f9922b2e916decd9386be4b5da7c668f756bd1e2
Tyler Porter <tylerpporter@gmail.com>
2020-02-19 09:47:07 -0700

    Create test_it_can_add_exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,10 +1,18 @@
 require_relative 'test_helper.rb'
 require './lib/museum.rb'
+require './lib/patron'
+require './lib/exhibit'
+
 
 class MuseumTest < Minitest::Test
 
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   def test_it_exists
@@ -14,6 +22,14 @@ class MuseumTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    assert_equal [@gems_and_minerals], @dmns.exhibits
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 
 end

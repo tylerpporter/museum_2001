a212291e74689bd253aec29d234cbe8c3a09e41c
Tyler Porter <tylerpporter@gmail.com>
2020-02-19 09:31:32 -0700

    Pass test_it_has_attributes

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,8 +1,10 @@
 class Patron
   attr_reader :name,
-              :spending_money
+              :spending_money,
+              :interests
   def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money.to_i
+    @interests = []
   end
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -14,6 +14,7 @@ class PatronTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
   end
 
 end

17562488464d378e9ea73401790f0547474f6a15
Tyler Porter <tylerpporter@gmail.com>
2020-02-19 09:39:30 -0700

    Create Museum class and test

---
+++ lib/museum.rb
@@ -0,0 +1,8 @@
+class Museum
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+  
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,14 @@
+require_relative 'test_helper.rb'
+require './lib/museum.rb'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+end

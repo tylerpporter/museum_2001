39ddaad223ebdbf46ee933fd333bb5b16bc43bc8
Tyler Porter <tylerpporter@gmail.com>
2020-02-19 09:29:29 -0700

    Create test_it_has_attributes

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,6 +1,6 @@
 class Patron
   def initialize(name, spending_money)
     @name = name
-    @spending_money = spending_money
+    @spending_money = spending_money.to_i
   end
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -11,4 +11,9 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, @patron_1
   end
 
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+  end
+
 end

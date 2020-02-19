641bb758ffe072da0f411021ff254afdd0a62a1c
Tyler Porter <tylerpporter@gmail.com>
2020-02-19 09:25:56 -0700

    Create initialize method and attr_readers

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,3 +1,8 @@
 class Exhibit
-
-end 
+  attr_reader :name,
+              :cost 
+  def initialize(exhibit_params)
+    @name = exhibit_params[:name]
+    @cost = exhibit_params[:cost].to_i
+  end
+end
